6b0a172bf59b94f1a2176598c8a01427e25073d4
Chris Moore <103774436+Elusid108@users.noreply.github.com>
2026-01-22 17:19:36 -0500

    Enable Google Sign-in on GitHub Pages: include config.js and remove .gitignore

---
+++ config.js
@@ -0,0 +1,8 @@
+const GOOGLE_CONFIG = {
+    CLIENT_ID: '623241453654-7b97s14dhno7g2mnhvr1n3lfb81gkiu8.apps.googleusercontent.com',
+    API_KEY: 'AIzaSyAx8q653Nckfh-FeLtlQp00GYE8mElEMbM',
+    SCOPES: [
+        'https://www.googleapis.com/auth/drive.appdata',
+        'https://www.googleapis.com/auth/drive.file'
+    ]
+};
